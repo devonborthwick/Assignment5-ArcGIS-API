1cef37348fda09a6c0a8834a5f2a5c8efbaea0db
devonborthwick <dborthw1@uwyo.edu>
2024-10-09 14:19:37 -0600

    big fix

--- map.js
+++ map.js
@@ -1,105 +1,180 @@
 var Main;
 
-require(
-    [
-        "esri/Map",
-        "esri/Graphic",
-        "esri/layers/GraphicsLayer",
-        "esri/layers/ElevationLayer",
-        "esri/views/SceneView"
-    ],
-    function(
-       Map, Graphic, GraphicsLayer, ElevationLayer, SceneView
-    ) {
-        $(document).ready(function() {
-            Main = (function() {
-                let layer = new ElevationLayer({
-                    url: "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
-                });
-                var map = new Map({
-                    basemap: "hybrid",
-                    ground: {
-                        layers: [layer]
-                    },
-                });
-    
-                var view = new SceneView({
-                    container: "map",
-                    viewingMode: "global",
-                    map: map,
-                    camera: {
-                        position: {
-                            x: -105.503,
-                            y: 44.270,
-                            z: 20000000,
-                            spatialReference: {
-                                wkid: 4326
-    
-                            }
-                        },
-                        heading: 0,
-                        tilt: 0
-                    },
-                    popup: {
-                        dockEnabled: true,
-                        dockOptions: {
-                            breakpoint: false
-                        }
-                    },
-                    // enable shadows to be cast from the features
-                    environment: {
-                        lighting: {
-                            directShadowsEnabled: false
-                        }
-                    }
-                })
-                const initMap = function(){
-               
-                   
-                    // var graphicsLayer = new GraphicsLayer()
-                    const graphicsLayer = new GraphicsLayer();
-                    map.add(graphicsLayer);
-                    for (const [key, value] of Object.entries(myStuff)){
-                        console.log(key, value)
-                        const point = {
-                            type: "point", 
-                            x: value.coord[0],
-                            y: value.coord[1],
-                            z: 10000
-                          };
-                  
-                          const markerSymbol = {
-                            type: "simple-marker", 
-                            color: [0, 0, 255],
-                            outline: {
-                              // autocasts as new SimpleLineSymbol()
-                              color: [255, 255, 255],
-                              width: 2
-                            }
-                          };
-                      
-                          const pointGraphic = new Graphic({
-                            geometry: point,
-                            symbol: markerSymbol,
-                            popupTemplate: {
-                                title: key + ": " + value.city + ", " + value.state
-                            }
-                          });
-                          graphicsLayer.add(pointGraphic);
-                    
-                    }
-                    
-                    
+require([
+  "esri/Map",
+  "esri/Graphic",
+  "esri/layers/GraphicsLayer",
+  "esri/layers/ElevationLayer",
+  "esri/views/SceneView",
+  "esri/widgets/Search",
+  "esri/geometry/Point"
+], function(
+  Map, Graphic, GraphicsLayer, ElevationLayer, SceneView, Search, Point
+) {
+  $(document).ready(function() {
+    Main = (function() {
+      let layer = new ElevationLayer({
+        url: "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"
+      });
+
+      let map = new Map({
+        basemap: "topo-vector",
+        ground: {
+          layers: [layer]
+        }
+      });
+
+      let view = new SceneView({
+        container: "map",
+        viewingMode: "global",
+        map: map,
+        camera: {
+          position: {
+            x: -105.58077771278067,
+            y: 41.31403134536375,
+            z: 2000000,
+            spatialReference: {
+              wkid: 4326
+            }
+          },
+          heading: 0,
+          tilt: 10
+        },
+        popup: {
+          dockEnabled: true,
+          dockOptions: {
+            breakpoint: false
+          }
+        },
+        environment: {
+          lighting: {
+            type: "sun"
+          },
+          atmosphereEnabled: true
+        }
+      });
+
+      const initMap = function() {
+
+
+        const graphicsLayer = new GraphicsLayer();
+        map.add(graphicsLayer);
+        for (const [key, value] of Object.entries(myStuff)) {
+          console.log("Processing point:", key, value.coord);
+          const point = {
+            type: "point",
+            x: value.coord[0],
+            y: value.coord[1],
+            z: 10000
+          };
+
+          const markerSymbol = {
+            type: "simple-marker",
+            style: "diamond",
+            color: [34, 139, 34],
+            outline: {
+              color: [255, 255, 255],
+              width: 2
+            }
+          };
+
+          const pointGraphic = new Graphic({
+            geometry: point,
+            symbol: markerSymbol,
+            popupTemplate: {
+              title: key + ": " + value.placename + ", " + value.date,
+              content: "These are some of the places I love to go on vacation."
+            }
+          });
+
+          graphicsLayer.add(pointGraphic);
+           
+        }
+
+        graphicsLayer.featureReduction = {
+          type: "selection",
+          selectionRadius: 100,
+          labelingInfo: [
+            {
+              deconflictionStrategy: "none",
+              labelExpressionInfo: {
+                expression: "Text($feature.cluster_count, '#,###')"
+              },
+              symbol: {
+                type: "text",
+                color: "white",
+                font: {
+                  family: "Noto Sans",
+                  size: "12px"
                 }
+              },
+              labelPlacement: "center-center"
+            }
+          ],
+          popupTemplate: {
+            title: "Cluster Summary",
+            content: "This cluster represents <b>{cluster_count}</b> features.",
+            fieldInfos: [
+              {
+                fieldName: "cluster _count",
+              format: {
+                places: 0,
+                digitSeparator: true
+                    }
+                }]
+                }};
+                
+                   
+                   view.on("click", function(event) {
+                    view.hitTest(event).then(function(response) {
+                      if (response.results.length) {
+                        const graphic = response.results.filter(result => result.graphic.geometry.type === "point")[0].graphic;
+                        if (graphic) {
+                          view.goTo({
+                            target: graphic.geometry, 
+                            zoom: 15 
+                          })
+                        }
+                      }
+                    })
+                  })
+              
+                  const source = [{
+                    layer: graphicsLayer,
+                    name: "Vacation Spots"
+                    
+              
+                   }];
+                    const searchWidget = new Search({
+                        view: view,
+                        sources: myStuff,
+                        placeholder: "Places",
+                        searchFields: ["placename", "city", "country"],
+                        displayField: "placename",
+                        outFields: ["placename", "city", "country"],
+                        exactMatch: false,
+                        zoomScale: 900,
+                        suggestionsEnabled: true,
+                        minSuggestCharacters: 0
+                        
+                      });
+                      view.ui.add(searchWidget, {
+                        position: "top-right"
+                      });
+                   
+                    
+                    };
+                  
+                    
+                
                 initMap()
                 return {
-           
-                };
+    
+                
 
-            })();
-        })
-
-    });
-
+            };
+            })()
+            })
+            });
 
     
--- obj.js
+++ obj.js
@@ -1,8 +1,15 @@
 var myStuff = {
-    "Graduate School": {city: "Fairbanks", state: "Alaska", "coord": [-147.776749, 64.835365] },
-    "Postdoc Place": {city: "Miami", state: "Florida", "coord": [-80.191788, 25.761681]},
-    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.502, 41.3104]},
-    "Favorite Vacation": {city: "San Diego", state: "California", "coord": [-117.161087, 32.715736] }
+    "Favorite Vacation": {country: "USA", city: "Gardiner", placename: "Yellowstone National Park", date:"07/2023", "coord": [-110.55936765144818, 44.649070188456726] },
+    "Most Common Vacation": {country: "USA", city: "Orlando", placename: "Walt Disney World", date:"Most Recent 05/2024", "coord": [-81.5707292747845, 28.377365028749193]},
+    "Furthest Traveled": {country: "Norway", placename: "Bergen", date:"07/2016", "coord": [5.31652986601545, 60.38785788158753]},
+    "Next Vacation": {country: "USA", city: "Bergen", placename: "Disney's Vero Beach Resort", date:"12/2024", "coord": [-80.39678854780999, 27.76177524128316] },
+    "Bucket List Vacation": {country: "Japan", city: "Tokyo", placename: "Tokyo", date:"TBA", "coord": [-35.6318880609463, 139.87746386620213]},
+    "Favorite Conference Trip":{country: "USA", city: "Honolulu", placename: "AAG 2024", date: "04/2024", "coord": [-157.8359133614524, 21.289904210207276]},
+    "Most Recent Trip":{country: "USA", city: "Moorcroft", placename: "Moorcroft, WY", date: "09/2024", "coord": [-104.70447027422514, 44.57448146905709]},
+    "Favorite Tourist Stops":{country: "USA", city: "Cape Canaveral", placename: "Kennedy Space Center",  date: "n/a", "coord": [-80.6816049766248, 28.52210466352875]},
+    "Recent Tourist Stops":{country: "USA", city: "Devil's Tower", placename: "Devil's Tower",  date: "n/a", "coord": [-104.71476895922677, 44.5910412599685]},
+    "Fun Tourist Stops":{country: "USA", city: "Honolulu", placename: "Diamond Head",  date: "n/a", "coord": [-157.80410503261803, 21.26398835036027]},
+    "Future Tourist Stops":{country: "USA", city: "Estes Park", placename: "Stanley Hotel", date: "TBA", "coord": [-105.52027420586803, 40.3769684595068]},
+    "Family Favorite Tourist Stop":{country: "USA", city: "Custer", placename: "Jewel Cave National Monument", date: "n/a", "coord": [-103.83006900493922, 43.73120658019018]},
 }
-
 
